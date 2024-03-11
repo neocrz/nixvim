bd49550ede592bdddaaec4a08849cd0b3f90ece2
neocrz <neo59crz@gmail.com>
2024-03-11 00:09:35 -0300

    basic config

--- config/default.nix
+++ config/default.nix
@@ -1,6 +1,8 @@
-{
+{pkgs, ...}:{
   # Import all your configuration modules here
   imports = [
-    ./bufferline.nix
+    ./plugins
+    ./options.nix
   ];
+  colorschemes.catppuccin.enable = true;
 }
---
+++ config/options.nix
@@ -0,0 +1,17 @@
+{
+  options = {
+    tabstop = 2;
+    shiftwidth = 2;
+    softtabstop = 2;
+    showtabline = 2;
+    smartindent = true;
+    expandtab = true;
+    
+    number = true;
+    relativenumber = true;
+    
+    mouse = "a";
+
+    cursorline = true;
+  };
+}
