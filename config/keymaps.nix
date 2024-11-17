1b33078d76c2a53a9b10531434da9f60e003daa3
neocrz <neo59crz@gmail.com>
2024-11-16 23:37:25 -0300

    keymaps delete, paste, copy to register

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -8,5 +8,58 @@
     "<leader>fh" = "help_tags";
     "<leader>fc" = "git_commits";
   };
+
+  keymaps = [
+    {
+      mode = ["n" "v"];
+      key = "<leader>D";
+      action = ''"_d'';
+      options = {desc = "Delete to void register";};
+    }
+
+    {
+      mode = "x";
+      key = "<leader>p";
+      action = ''"_dP'';
+      options = {desc = "Deletes to void register and paste over";};
+    }
+    
+    {
+      mode = ["n" "v"];
+      key = "<leader>y";
+      action = ''"+y'';
+      options = {desc = "Copy to system clipboard";};
+    }
+
+    {
+      mode = ["n" "v"];
+      key = "<leader>Y";
+      action = ''"+Y'';
+      options = {desc = "Copy to system clipboard";};
+    }
+
+
+    # <C-hjkl> to move in insert mode
+    {
+      action = "<Left>";
+      key = "<C-h>";
+      mode = "i";
+    }
+    {
+      action = "<Right>";
+      key = "<C-l>";
+      mode = "i";
+    }
+    {
+      action = "<Up>";
+      key = "<C-k>";
+      mode = "i";
+    }
+    {
+      action = "<Down>";
+      key = "<C-j>";
+      mode = "i";
+    }
+  ];
 }
 
